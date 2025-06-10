7a4f4e00f50c1b3fb73c0f33249c5b02e4177e0e
kaoozhi <kaoozhi@gmail.com>
2025-06-10 10:21:14 +0200

    checkout back state of lab1commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,17 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Collatz sequence:
+     * if n is even, then next number is n /2
+     * if n is odd, then next number is 3*n +1
+     * if n is 1, then next number is 1 ! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 != 0) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
